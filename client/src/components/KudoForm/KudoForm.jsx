02aec6c8434b56a375badb34026a923fde3d759f
Nathaniel <nielb4all@gmail.com>
2022-05-15 15:01:29 -0700

    Added first form textfield

--- client/src/components/KudoForm/KudoForm.jsx
+++ client/src/components/KudoForm/KudoForm.jsx
@@ -1,57 +1,70 @@
-import React from "react";
-import { FormControl, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Card, FormControl, Grid, TextField, Typography } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import placeholder from "../../assets/image-placeholder.jpeg";
+import CardContent from "@mui/material/CardContent";
 import { Button } from "@mui/material";
 
 // THE COMMENT FORM ON THE VIDEO PAGE
 function KudoForm() {
+  const [values, setValues] = useState({
+    name: "",
+    department: "",
+    comment: "",
+  });
+
   return (
-    <section
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems:'center',
-        justifyContent :'',
-        maxWidth: 280,
-        minWidth: 280,
-      }}
-    >
-      {/* <====================> FORM <====================> */}
-      <FormControl>
-        <Typography gutterBottom varient="h3" align="center">
-          KUDO
-        </Typography>
-      </FormControl>
-      {/* <====================> IMAGE <====================> */}
-      <CardMedia
-        sx={{ width: "90%" }}
-        component="img"
-        height="194"
-        image={placeholder}
-        alt="placeholder image"
-      />
-      {/* <====================> SUBMIT KUDO <====================> */}
-      <Button
-        href="/create-kudo"
-        variant="contained"
+    <Card>
+      <CardContent
         sx={{
-          fontSize: 16,
-          padding: 1,
-          color: "white",
-          "&:hover": {
-            color: "#37cd7b",
-            backgroundColor: "white",
-          },
-          bgcolor: "#37cd7b",
-          borderTopLeftRadius: "5rem",
-          borderBottomLeftRadius: "5rem",
-          borderBottomRightRadius: "5rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "",
+          maxWidth: 280,
+          minWidth: 280,
         }}
       >
-        POST KUDO
-      </Button>
-    </section>
+        {/* <====================> FORM <====================> */}
+        <Grid container spacing={1}>
+          <Grid item>
+            <TextField label='Recipient' placeholder="Select Recipient" />
+
+            
+          </Grid>
+
+        </Grid>
+        
+        {/* <====================> IMAGE <====================> */}
+        <CardMedia
+          sx={{ width: "90%" }}
+          component="img"
+          height="194"
+          image={placeholder}
+          alt="placeholder image"
+        />
+        {/* <====================> SUBMIT KUDO <====================> */}
+        <Button
+          href="/create-kudo"
+          variant="contained"
+          sx={{
+            fontSize: 16,
+            padding: 1,
+            color: "white",
+            "&:hover": {
+              color: "#37cd7b",
+              backgroundColor: "white",
+            },
+            bgcolor: "#37cd7b",
+            borderTopLeftRadius: "5rem",
+            borderBottomLeftRadius: "5rem",
+            borderBottomRightRadius: "5rem",
+          }}
+        >
+          POST KUDO
+        </Button>
+      </CardContent>
+    </Card>
     // <section className='form-card'>
     //   <section className='form-container'>
     //     <img className='form-image' alt='user portrait'  src={profileImg} />
