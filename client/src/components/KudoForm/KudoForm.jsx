9166e73f0850bcd77db2cfec433d292a69fe9651
Nathaniel <nielb4all@gmail.com>
2022-05-16 09:18:41 -0700

    Form size & centering fixed

--- client/src/components/KudoForm/KudoForm.jsx
+++ client/src/components/KudoForm/KudoForm.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Grid, TextField } from "@mui/material";
+import { Grid } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import placeholder from "../../assets/image-placeholder.jpeg";
 import { Button } from "@mui/material";
 import { useForm, Form } from "../useForm";
 import { CardContent } from "@mui/material";
 import Input from "../controls/Input";
-import Select from "../controls/Select";
 import * as employees from "../../employees/employees";
 
 /* <====================> DEFINED FORM PROPERTIES <====================> */
@@ -22,7 +21,7 @@ export default function KudoForm() {
   const { values, setValues, handleInputChange } = useForm(initialFValues);
 
   return (
-    <Form
+    <CardContent
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -31,93 +30,91 @@ export default function KudoForm() {
         // width: "100%",
       }}
     >
-      {/* <====================> FORM CONTAINER <====================> */}
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          my: ".5rem",
-        }}
-      >
-        {/* <==========> RECIPIENT <==========> */}
+      <Form>
+        {/* <====================> FORM CONTAINER <====================> */}
         <Grid
-          item
-          // xs={8}
+          container
           sx={{
-            mb: ".5rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            my: ".5rem",
           }}
         >
-          <Input
-            label="Recipient"
-            placeholder="Select Recipient"
-            name="Full Name"
-            value={values.fullName}
-            onChange={handleInputChange}
-            options={[employees.getEmployees]}
-            // style={{ width: 200 }}
+          {/* <==========> RECIPIENT <==========> */}
+          <Grid
+            item
+            // xs={8}
+            sx={{
+              mb: ".5rem",
+            }}
+          >
+            <Input
+              label="Recipient"
+              placeholder="Select Recipient"
+              name="Full Name"
+              value={values.fullName}
+              onChange={handleInputChange}
+              options={[employees.getEmployees]}
+              // style={{ width: 200 }}
+            />
+          </Grid>
+
+          {/* <==========> KUDO <==========> */}
+          <Grid
+            item
+            // xs={8}
+            sx={{
+              mb: ".5rem",
+              
+            }}
+          >
+            <Input
+              variant="outlined"
+              label="Kudo"
+              name="Kudo"
+              placeholder="Leave your Kudo here!"
+              value={values.kudo}
+              onChange={handleInputChange}
+            />
+          </Grid>
+        </Grid>
+
+        {/* <====================> IMAGE <====================> */}
+        <Grid>
+          <CardMedia
+            component="img"
+            height="194"
+            image={placeholder}
+            alt="placeholder image"
           />
         </Grid>
 
-        {/* <==========> KUDO <==========> */}
-        <Grid
-          item
-          // xs={8}
+        {/* <====================> SUBMIT KUDO <====================> */}
+        <Button
+          href="/create-kudo"
+          variant="contained"
+          type="submit"
           sx={{
-            mb: ".5rem",
+            display:'flex',
+            alignItems:'center',
+            my: "1rem",
+            fontSize: 16,
+            padding: 1,
+            color: "white",
+            "&:hover": {
+              color: "#37cd7b",
+              backgroundColor: "white",
+            },
+            bgcolor: "#37cd7b",
+            borderTopLeftRadius: "5rem",
+            borderBottomLeftRadius: "5rem",
+            borderBottomRightRadius: "5rem",
           }}
         >
-          <Input
-            variant="outlined"
-            label="Kudo"
-            name="Kudo"
-            placeholder="Leave your Kudo here!"
-            value={values.kudo}
-            onChange={handleInputChange}
-          />
-        </Grid>
-      </Grid>
-
-      {/* <====================> IMAGE <====================> */}
-      <Grid
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "90%",
-          ml: '.5rem',
-        }}
-      >
-        <CardMedia
-          component="img"
-          height="194"
-          image={placeholder}
-          alt="placeholder image"
-        />
-      </Grid>
-
-      {/* <====================> SUBMIT KUDO <====================> */}
-      <Button
-        href="/create-kudo"
-        variant="contained"
-        sx={{
-          my: "1rem",
-          fontSize: 16,
-          padding: 1,
-          color: "white",
-          "&:hover": {
-            color: "#37cd7b",
-            backgroundColor: "white",
-          },
-          bgcolor: "#37cd7b",
-          borderTopLeftRadius: "5rem",
-          borderBottomLeftRadius: "5rem",
-          borderBottomRightRadius: "5rem",
-        }}
-      >
-        POST KUDO
-      </Button>
-    </Form>
+          POST KUDO
+        </Button>
+      </Form>
+    </CardContent>
   );
 }
