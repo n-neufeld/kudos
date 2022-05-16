169e40de5b0422928343dbdc028b399c66711816
Nathaniel <nielb4all@gmail.com>
2022-05-16 00:01:57 -0700

    Added a selection dropdown but something is broken at the moment

--- client/src/components/controls/Select.js
+++ client/src/components/controls/Select.js
@@ -2,7 +2,6 @@ import {
   FormControl,
   InputLabel,
   MenuItem,
-  Menu,
   Select as MuiSelect,
 } from "@mui/material";
 import React from "react";
@@ -11,15 +10,20 @@ export default function Select(props) {
   const { name, label, value, onChange, option } = props;
 
   return (
-    <FormControl variant="outlined">
+    <FormControl 
+    variant="outlined">
       <InputLabel>{label}</InputLabel>
-      <MuiSelect name={name} label={label} value={value} onChange={onChange}>
-        <Menu value="">None</Menu>
-        {option.map((item) => (
-          <MenuItem key={item.id} value={item.id}>
-            {item.title}
-          </MenuItem>
-        ))}
+      <MuiSelect 
+        name={name} 
+        label={label} 
+        value={value} 
+        onChange={onChange}>
+          <MenuItem value="">None</MenuItem>
+          {
+            option.map(
+            item => (<MenuItem key={item.id} value={item.id}>{item.title}</MenuItem>)
+            )
+        }
       </MuiSelect>
     </FormControl>
   );
