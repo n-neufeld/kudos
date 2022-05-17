0f3be8c0fd5eed3836420649c0403853cb6adfbf
Nathaniel <nielb4all@gmail.com>
2022-05-16 21:57:55 -0700

    Added employee json

--- client/src/employees/employees.js
+++ client/src/employees/employees.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const getEmployees = () => ([
-    {id: '1', title: 'Ricky Bobby'},
-    {id: '1', title: 'Mark Michael'},
-    {id: '1', title: 'Clark Kat'},
-    {id: '1', title: 'Brick Wayne'},
+    {id: '1', name: 'Ricky Bobby'},
+    {id: '1', name: 'Mark Michael'},
+    {id: '1', name: 'Clark Kat'},
+    {id: '1', name: 'Brick Wayne'},
 ])
